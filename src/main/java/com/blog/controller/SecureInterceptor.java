b0201a46c5f77461ee16433d591ef30d4a31a25c
liaoshanggang <78788760@qq.com>
2018-01-30 18:38:15 +0800

    Add interceptors and filters

---
+++ src/main/java/com/blog/controller/SecureInterceptor.java
@@ -0,0 +1,28 @@
+package com.blog.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+/**
+ * 安全访问拦截器
+ *
+ */
+public class SecureInterceptor extends HandlerInterceptorAdapter {
+	static Logger logger = Logger.getLogger(MyHandlerInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		logger.info("1、preHandle():"+request.getRequestURL());
+		HttpSession session = request.getSession();
+		if(session.getAttribute("logUser") == null){
+			response.sendRedirect(request.getContextPath() + "/login.jsp");
+			logger.info(request.getContextPath() + "/login.jsp");
+			return false;
+		}
+		return true;
+	}
+}
